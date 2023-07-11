1013613a03e5afcfb6de5018e915510a899127ba
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2023-07-12 00:13:35 +0100

    amabassadot typeform embed

---
+++ src/pages/ambassadors/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import HeadComp from "../../components/MetaData";
+
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
+import { Widget } from '@typeform/embed-react'
+
+export default function index() {
+  return (
+    <div>
+      <HeadComp
+        title="Luca's Ambassador Program"
+      />
+      <Navbar />
+
+      <div className="headerA" style={{ width: '100%', height: '95vh' }}>
+        <Widget id="szUGIH1h" style={{ width: '100%', height: '100%' }} className="my-form" />
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
